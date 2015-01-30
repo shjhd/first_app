84781b7c78d22ff6aa0d6b740a4405284c028c02
xtunes <xtunes@xtunes1312.local>
2015-01-30 11:14:01 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '94c864896de8583afbf95af9a144a8b1aa5ad50b3d2b6025ff22d1310475d5753159f479ee7be1b1f306af2791c0076b6103a84aa2fc33d1fbb00fdd21ac3485'
